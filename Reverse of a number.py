fc584625d87fec4b4a52e0e24f178dcfecadc63b
CHIRAG3899 <66902259+CHIRAG3899@users.noreply.github.com>
2021-06-06 16:22:27 +0530

    Add files via upload

---
+++ Reverse of a number.py
@@ -0,0 +1,24 @@
+#Reverse of a number
+#Write a program to generate the reverse of a given number N.
+#Print the corresponding reverse number.
+#Note : If a number has trailing zeros, then its reverse will not include them. For e.g., reverse of 10400 will be 401 instead of 00401.
+#Input format :
+#Integer N
+#Output format :
+#Corresponding reverse number
+#Sample Input 1 :
+#1234
+#Sample Output 1 :
+#4321
+#Sample Input 2 :
+#1980
+#Sample Output 2 :
+#891
+
+n=int(input())
+rev=0
+while(n>0):
+    lg=n%10
+    rev=(rev*10)+lg
+    n=n//10
+print(rev)
